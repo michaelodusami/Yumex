81e34f773c6cc0e6197c4a55f9d6dd0a9d26c4c5
modusami <128193218+modusami@users.noreply.github.com>
2024-04-12 19:16:37 -0400

    Update Layout For Forum

--- src/app/ui/homepage/Forum.tsx
+++ src/app/ui/homepage/Forum.tsx
@@ -13,13 +13,18 @@ return (
     <div className="mb-8">
         <div className="h-8 w-full rounded bg-gray-200 dark:bg-white animate-pulse"></div>
     </div>
-    <div className="grid grid-cols-1 gap-4">
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
     {posts.map((post) => (
-          <div key={post.id} className="rounded-lg dark:shadow-neutral-800 p-4 shadow">
+          <div key={post.id} className="rounded-lg dark:shadow-neutral-800 p-4 shadow relative">
+            {/* food image */}
             <Image src={post.image} width={1296} height={728}  alt={post.title} className="mb-4 h-48 w-full rounded object-cover md:h-[20rem] lg:h-[30rem]"/>
+            {/* title */}
             <h2 className="mb-2 text-xl font-bold">{post.title}</h2>
+            {/* content (description) */}
             <p className="mb-4">{post.content}</p>
+            {/* user information */}
             <div className="flex items-center space-x-2">
+              {/* user profile pic */}
               <Image
                 src={"/userlogo.png"}
                 alt={getUserName(post.user_id)}
@@ -27,7 +32,9 @@ return (
                 height={30}
                 className="h-8 w-8 rounded-full"
               />
+              {/* user who created it */}
               <span className="">{getUserName(post.user_id)}</span>
+              {/* category tag of what type of food it is */}
             </div>
           </div>
         ))}
