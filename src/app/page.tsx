f4ee36a5fb8cdd4d11cbe59d4ccefd8e12e96aa7
modusami <128193218+modusami@users.noreply.github.com>
2024-04-12 11:47:13 -0400

    Update Page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import { poppins } from "./ui/fonts";
 
 export default function Home() {
   return (
-    <main className="min-h-screen p-24">
-   
+    <main className="min-h-screen">
+        <div className="w-full min-h-screen mx-auto shadow-lg flex-col flex">
+            <div className="text-center w-full">
+                <h1 className={poppins.className + " text-[120px]"}>Yumex.</h1>
+            </div>
+            <div className="w-full">
+              
+            </div>
+        </div>
     </main>
   );
 }
