5cb1c9788f7145e3b70672d3b6b0d3257cd574c5
modusami <128193218+modusami@users.noreply.github.com>
2024-04-12 11:18:52 -0400

    Add Fonts To Page Ready For Export

---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
+
+export const inter = Inter({subsets: ["latin"]})
+export const poppins = Poppins({weight: ["400", "700", "900"], subsets:["latin"]})
