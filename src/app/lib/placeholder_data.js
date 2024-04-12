85823a40e3557bff793508e46e2d9507f214cf5c
modusami <128193218+modusami@users.noreply.github.com>
2024-04-12 19:16:22 -0400

    Update placeholder data

--- src/app/lib/placeholder_data.js
+++ src/app/lib/placeholder_data.js
@@ -11,6 +11,12 @@ export const users = [
 		email: "use2r@nextmail.com",
 		password: "1234561",
 	},
+	{
+		user_id: "410044b2-4001-42e1-9855-fec4b6a6442a",
+		username: "Dormamu",
+		email: "user4@nextmail.com",
+		password: "12345612",
+	},
 ];
 
 export const posts = [
@@ -43,6 +49,39 @@ export const posts = [
 		content:
 			'Behold the "Heavenly Dream Cake" - a decadent creation that transcends the boundaries of ordinary confectionery. Imagine layers of moist, velvety sponge cake, lovingly enrobed in a blanket of rich, creamy frosting that whispers promises of pure indulgence. With each forkful, youre transported to a realm of pure bliss, where the sweetness dances on your palate like a melody, and every bite is a journey to the celestial heights of dessert perfection. Its not just a cake. its a divine revelation that leaves you yearning for just one more heavenly slice.',
 		image: "/examplecake.jpg",
+		createdAt: new Date("2023-05-17T09:00:00"),
+		upvotes: 0,
+		comments: [],
+	},
+	{
+		id: "3122w3e4r-5t6y-7u8i-9o0p-1a2s3d4f5g6h",
+		user_id: users[2].user_id,
+		title: "Savor the Crunch: Embark on a Flavorful Journey with Our Irresistibly Golden, Perfectly Seasoned Fries - A Culinary Delight for Fry Enthusiasts Everywhere",
+		content:
+			"Crispy batons of golden perfection, seasoned to savory perfection and served hot.",
+		image: "/examplefries.jpg",
+		createdAt: new Date("2023-05-17T09:00:00"),
+		upvotes: 0,
+		comments: [],
+	},
+	{
+		id: "3q1232w3e4r-5t6y-7u8i-9o0p-1a2s3d4f5g6h",
+		user_id: users[1].user_id,
+		title: "Strawberry Glazed Donuts Cooked All The Way To Perfection, Amazing 🍩!!",
+		content:
+			"Fluffy clouds of fried dough, glazed to perfection and adorned with a sprinkle of sweetness.",
+		image: "/exampledonut.jpg",
+		createdAt: new Date("2023-05-17T09:00:00"),
+		upvotes: 0,
+		comments: [],
+	},
+	{
+		id: "31234q2w3e4r-5t6y-7u8i-9o0p-1a2s3d4f5g6h",
+		user_id: users[0].user_id,
+		title: "Japanse Toast 🍞 ",
+		content:
+			"Slices of bread, lightly toasted to a golden hue, offering a warm and comforting crunch with every bite.",
+		image: "/exampletoast.jpg",
 		createdAt: new Date("2023-05-17T09:00:00"),
 		upvotes: 0,
 		comments: [],
@@ -82,6 +121,13 @@ export const populateCommentsAndUpvotes = () => {
 			postId: posts[2].id,
 			content:
 				"Thank you! I've been painting for about 5 years now. It's a great way to express myself.",
+			createdAt: new Date("2023-05-17T11:10:00"),
+		},
+		{
+			user_id: users[1].id,
+			postId: posts[3].id,
+			content:
+				"I've been painting for about 5 years now. It's a great way to express myself.",
 			createdAt: new Date("2023-05-17T11:10:00"),
 		},
 	];
