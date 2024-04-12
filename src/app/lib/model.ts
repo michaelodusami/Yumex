05bc83ea0fd8b94a263ef6ffea431c5d7cc7ac31
modusami <128193218+modusami@users.noreply.github.com>
2024-04-12 11:10:07 -0400

    Initial Files

---
+++ src/app/lib/model.ts
@@ -0,0 +1,15 @@
+// This file contains type definitions for your data.
+// It describes the shape of the data, and what data type each property should accept.
+/**
+ * EXAMPLE:
+ * export type User = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+};
+ */
+
+export type User = {
+    id: string;
+}
