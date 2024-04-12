b0cbfb80217c9713f5621fc44490524f4f8427d1
modusami <128193218+modusami@users.noreply.github.com>
2024-04-12 11:57:25 -0400

    Update For small screens

--- src/app/ui/nav.tsx
+++ src/app/ui/nav.tsx
@@ -4,22 +4,22 @@ import { poppins } from "./fonts";
 
 export default function Nav() {
   return (
-    <div>
+
         
-        <nav className="flex w-full items-center justify-between bg-white p-4">
-        <div className="flex items-center w-[20%]">
+        <nav className="flex w-full items-center justify-between  bg-white p-4 ">
+        <div className="md:flex items-center w-[20%] hidden">
             <Link href="/create" className={poppins.className + " text-gray-800 hover:text-gray-600"}>
             Create a post
             </Link>
         </div>
-        <div className="text-2xl flex-1 text-center font-bold text-gray-800">
+        <div className="text-2xl flex-1 md:text-center font-bold text-gray-800">
             <Link href={"/"} className={poppins.className}>
                 Yumex
             </Link>
         </div>
         <Search placeholder="Search..." />
         </nav>
-    </div>
+ 
     
   );
 }
