b442087e71d2d5a3e02ac50b47505cb1a795a7d8
modusami <128193218+modusami@users.noreply.github.com>
2024-04-12 21:38:45 -0400

    Update Page.jsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import { ForumSkeleton } from "./ui/skeletons";
-import {posts, users} from "@/app/lib/placeholder_data";
 import Forum from "./ui/homepage/Forum";
 
 export default function Home() {
@@ -8,7 +7,7 @@ export default function Home() {
     <main className="min-h-screen">
         <div className="w-full lg:w-[100%] min-h-screen mx-auto flex-col flex">
             {/* <ForumSkeleton/> */}
-            <Forum users={users} posts={posts}/>
+            <Forum/>
         </div>
     </main>
   );
