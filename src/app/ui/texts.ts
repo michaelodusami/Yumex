4ce8e7c0f40685e02889efd4ee5ebb84affac3d3
modusami <128193218+modusami@users.noreply.github.com>
2024-04-13 15:33:58 -0400

    Add more default text

--- src/app/ui/texts.ts
+++ src/app/ui/texts.ts
@@ -1,2 +1,6 @@
+export const defaultContentText = "...";
 
-export const defaultContentText = "..."
+export const postPreviewContentText =
+	"This is a sample description which servers as the content for your post.";
+export const postPreviewTitleText = "Sample Title Belongs Here...";
+export const postPreviewImageFile = "/exampletoast.jpg";
