3a266b38355211902ebce1d4ac8820a88cb4956c
modusami <128193218+modusami@users.noreply.github.com>
2024-04-12 21:39:02 -0400

    Update Forum.tsx

--- src/app/ui/homepage/Forum.tsx
+++ src/app/ui/homepage/Forum.tsx
@@ -1,23 +1,45 @@
+'use client';
+
 import {Post, User} from "../../lib/model"
-import { getUserName, getFormattedDate } from "@/app/lib/utils";
+import { getUserName, getFormattedDate, handleUpvote } from "@/app/lib/utils";
 import Image from "next/image";
 import { clsx } from "clsx";
 import { categoryColors } from "../colors";
 import {defaultContentText} from "../texts";
+import { ChevronDoubleUpIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+import {posts} from "@/app/lib/placeholder_data";
 
 interface ForumProps {
     posts: Post[];
     users: User[];
 }
 
-const Forum: React.FC<ForumProps> = ({posts, users}) => {
+const Forum: React.FC = () => {
+
+const [initialPosts, setPosts] = useState(posts);
+
+const handleUpvote = (postId: string) => {
+  setPosts((prevPosts) => {
+    return prevPosts.map((post) => {
+      if (post.id === postId) {
+        return {
+          ...post,
+          upvotes: post.upvotes + 1,
+        };
+      }
+      return post;
+    });
+  });
+};
+
 return (
     <div className="container mx-auto px-4 py-8">
     <div className="mb-8">
         <div className="h-8 w-full rounded bg-gray-200 dark:bg-white animate-pulse"></div>
     </div>
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-    {posts.map((post) => (
+    {initialPosts.map((post) => (
           <div key={post.id} className="rounded-lg dark:shadow-neutral-800 p-4 shadow relative">
             {/* food image */}
             <Image src={post.image} width={1296} height={728}  alt={post.title} className="mb-4 h-48 w-full rounded object-cover md:h-[20rem] lg:h-[30rem]"/>
@@ -26,7 +48,15 @@ return (
             {/* content (description) */}
             <p className={clsx("mb-4 line-clamp-2", {"text-white dark:text-black" : post.content === ""})}>{post.content ? post.content : defaultContentText}</p>
             {/* user information */}
-            <p className="text-sm">Posted At: {getFormattedDate(post.createdAt)}</p>
+            <div className="w-full flex justify-between">
+              <p>Upvotes: {post.upvotes}</p>
+              <button onClick={(e) => {e.stopPropagation(); handleUpvote(post.id)}}>
+              <ChevronDoubleUpIcon className="h-30px] w-[30px] cursor-pointer" />
+              </button>
+              
+            </div>
+            
+            <p className="text-sm italic">Posted At: {getFormattedDate(post.createdAt)}</p>
             <div className="flex items-center space-x-2 mt-5">
               {/* user profile pic */}
               <Image
@@ -49,5 +79,6 @@ return (
     </div>
 );
 };
+
 
 export default Forum;
