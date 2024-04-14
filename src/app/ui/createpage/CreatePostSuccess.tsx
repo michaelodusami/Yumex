9f42eb2c3e7ccad86c0b69ae0de7cda546f0f030
modusami <128193218+modusami@users.noreply.github.com>
2024-04-14 17:07:04 -0400

    Create post creation page

---
+++ src/app/ui/createpage/CreatePostSuccess.tsx
@@ -0,0 +1,16 @@
+import { poppins } from "../fonts";
+
+const CreatePostSuccess: React.FC = () => {
+	return (
+		<div className="text-[2rem] md:text-[2rem] lg:text-[3rem] w-full min-h-[90vh] flex justify-center items-center transition-all">
+			<div className="text-center">
+				<p className="font-bold">Post Created!</p>
+				<span className="text-[1rem] md:text-[1.5rem] lg:text-[2rem]">
+					sending to post page...
+				</span>
+			</div>
+		</div>
+	);
+};
+
+export default CreatePostSuccess;
