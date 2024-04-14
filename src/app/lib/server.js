b00ff8424f983af4aca497a1946c7b06c0b805c8
modusami <128193218+modusami@users.noreply.github.com>
2024-04-14 18:56:14 -0400

    Add Supabase Admin

--- src/app/lib/server.js
+++ src/app/lib/server.js
@@ -2,5 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 const URL = process.env.NEXT_PUBLIC_BASE_URL;
 const ANON = process.env.NEXT_PUBLIC_ANON_KEY;
-
+const SERVICEKEY = process.env.NEXT_PUBLIC_SERVICE_ROLE_KEY;
 export const supabase = createClient(URL, ANON);
+export const supabaseAdmin = createClient(URL, SERVICEKEY);
