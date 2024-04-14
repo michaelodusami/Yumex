2147f18220281629a95c2b5d234a60d9f99137b3
modusami <128193218+modusami@users.noreply.github.com>
2024-04-14 16:52:23 -0400

    Add Data Methods For Upaloding Posts

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,10 +1,57 @@
 import { supabase } from "./server";
+import { bucketName, bucketPostFolderName } from "./common_names";
 
+/**
+ * Gets the user from the database
+ * @returns user from database
+ */
+export const getUserFromDatabase = async () => {
+	const {
+		data: { user },
+	} = await supabase.auth.getUser();
+	return user;
+};
+
+export const getEmailFromUser = async () => {
+	const userEmail = await getUserFromDatabase().then((data) => data?.email);
+	if (userEmail) {
+		return userEmail;
+	}
+	return null;
+};
+
+/**
+ * Fetches posts from the database
+ * @returns supabase post data
+ */
 export const fetchPostsFromDatabase = async () => {
 	const { data } = await supabase.from("Posts").select().order("created_at", { ascending: true });
 	return data;
 };
 
+/**
+ * Sends user information to the database
+ * @param userObjectData at post creation
+ */
 export const createPostToDatabase = async (userObjectData: Object) => {
 	await supabase.from("Posts").insert(userObjectData).select();
+};
+
+/**
+ *
+ * @param file the user wants to add as a post
+ * @returns file name
+ */
+export const uploadFile = async (file: File | null, filePath: string) => {
+	if (file) {
+		const { data, error } = await supabase.storage
+			.from(bucketName + "/" + bucketPostFolderName)
+			.upload(filePath, file);
+		if (error) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	return false;
 };
