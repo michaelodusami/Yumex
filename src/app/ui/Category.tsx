1edbf69ba3c5940b06d81743169a96e024e641f3
modusami <128193218+modusami@users.noreply.github.com>
2024-04-14 21:35:27 -0400

    Make Category Component

---
+++ src/app/ui/Category.tsx
@@ -0,0 +1,16 @@
+import { categoryColors } from "./colors";
+
+const Category: React.FC<{ category: string }> = ({ category = "NONE" }) => {
+	return (
+		<span
+			style={{
+				backgroundColor: categoryColors[category !== "" ? category : "asian"],
+			}}
+			className="uppercase border rounded-lg p-2 text-black"
+		>
+			{category !== "" ? category : "asian"}
+		</span>
+	);
+};
+
+export default Category;
