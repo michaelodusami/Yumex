56224a903cfd6d8971dcbf779ed2cbacac586f5f
modusami <128193218+modusami@users.noreply.github.com>
2024-04-16 11:26:20 -0400

    Add size

--- src/app/ui/sizes.ts
+++ src/app/ui/sizes.ts
@@ -1,7 +1,6 @@
-export const phoneIconWH = "w-[20px] h-[20px]";
-export const phoneAvatarIconDropdownWH = "w-[35px] h-[35px]";
-
-export const navMediumWidth = " md:w-[90%]";
-export const navLargeWidth = " lg:w-[80%]";
-
-export const singleColWidth = " md:w-[90%] lg:w-[60%]";
+export const phoneIconWH = "w-[20px] h-[20px] ";
+export const phoneAvatarIconDropdownWH = "w-[35px] h-[35px] ";
+export const navMediumWidth = " md:w-[90%] ";
+export const navLargeWidth = " lg:w-[80%] ";
+export const singleColWidth = " md:w-[90%] lg:w-[60%] ";
+export const POST_MIN_MAX_HEIGHT = " min-h-32 max-h-32 ";
