0870ce6a032c795dd66ff153a7bfc7552fe75235
modusami <128193218+modusami@users.noreply.github.com>
2024-04-16 11:26:44 -0400

    Update model.ts

--- src/app/lib/model.ts
+++ src/app/lib/model.ts
@@ -1,6 +1,10 @@
 // model.ts
 // this file represents static data used throughout the application
 
+/**
+ * Object containing various food categories.
+ * Each category is represented by a key-value pair where the key is the category name and the value is a string representation of the category.
+ */
 export const food_categories = {
 	asian: "asian",
 	mexican: "mexican",
@@ -10,4 +14,14 @@ export const food_categories = {
 	dessert: "dessert",
 	healthy: "healthy",
 	pastry: "pastry",
+};
+
+/**
+ * Object containing various routes used in the application.
+ * Each route is represented by a key-value pair where the key is the route name and the value is the corresponding route path.
+ */
+export const all_routes = {
+	home: "/",
+	create: "/create",
+	post: "/posts/",
 };
