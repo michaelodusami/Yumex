c37499d942279608d425c1114f8520aa0dab6798
modusami <128193218+modusami@users.noreply.github.com>
2024-04-16 11:09:47 -0400

    Update data.ts

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,9 +1,15 @@
 import { supabase, supabaseAdmin } from "./server";
 import { bucketName } from "./common_names";
 
+/*****************************************************
+ *
+ *  User Related Methods
+ *
+ *****************************************************/
+
 /**
- * Gets the user from the database
- * @returns user from database
+ * Retrieves the user from the current session in the database.
+ * @returns {Promise<object>} The user object from the database.
  */
 export const getUserFromCurrentSessionFromDatabase = async () => {
 	const {
@@ -13,10 +19,10 @@ export const getUserFromCurrentSessionFromDatabase = async () => {
 };
 
 /**
- * Gets the user email or id from the database
- * @param id of user
- * @param infoType the user wants to return
- * @returns the user's infotype or data if infoType is not among those in a switch statement
+ * Retrieves the user email or id from the database.
+ * @param {string} id - The id of the user.
+ * @param {string} infoType - The type of information to retrieve (email or id).
+ * @returns {Promise<string | object>} The user's email or id based on the infoType parameter.
  */
 export const getUserInfoByIdFromDatabase = async (id: string, infoType: string) => {
 	const { data, error } = await supabaseAdmin.auth.admin.getUserById(id);
@@ -30,8 +36,8 @@ export const getUserInfoByIdFromDatabase = async (id: string, infoType: string) 
 };
 
 /**
- * Gets email of the current user in the session
- * @returns email of the current user
+ * Retrieves the email of the current user from the session.
+ * @returns {Promise<string | null>} The email of the current user, or null if not available.
  */
 export const getEmailFromUser = async () => {
 	const userEmail = await getUserFromCurrentSessionFromDatabase().then((data) => data?.email);
@@ -42,8 +48,8 @@ export const getEmailFromUser = async () => {
 };
 
 /**
- * Gets the user's id from the current session
- * @returns the user id from the current session
+ * Retrieves the user id from the current session.
+ * @returns {Promise<string | null>} The user id from the current session, or null if not available.
  */
 export const getIdFromUser = async () => {
 	const userId = await getUserFromCurrentSessionFromDatabase().then((data) => data?.id);
@@ -52,10 +58,17 @@ export const getIdFromUser = async () => {
 	}
 	return null;
 };
+
+/*****************************************************
+ *
+ *  Post Related Methods
+ *
+ *****************************************************/
+
 /**
- * Fetches post from database
- * @param sortOption
- * @returns posts
+ * Fetches posts from the database based on the specified sorting option.
+ * @param {string} sortOption - The sorting option for posts (e.g., "last created", "upvotes").
+ * @returns {Promise<object[]>} An array of posts fetched from the database.
  */
 export const fetchPostsFromDatabase = async (sortOption: string) => {
 	const { data } = await supabase
@@ -68,9 +81,9 @@ export const fetchPostsFromDatabase = async (sortOption: string) => {
 };
 
 /**
- * Gets user image from the bucket
- * @param filePath
- * @returns
+ * Retrieves the public URL of a post image from the storage bucket.
+ * @param {string} filePath - The file path of the post image.
+ * @returns {Promise<string>} The public URL of the post image.
  */
 export const fetchPostImageFromDatbase = async (filePath: string) => {
 	const { data } = supabase.storage.from(bucketName).getPublicUrl(filePath);
@@ -78,17 +91,18 @@ export const fetchPostImageFromDatbase = async (filePath: string) => {
 };
 
 /**
- * Sends user information to the database
- * @param userObjectData at post creation
+ * Creates a new post in the database.
+ * @param {Object} userObjectData - Data of the post to be created.
  */
 export const createPostToDatabase = async (userObjectData: Object) => {
 	await supabase.from("Posts").insert(userObjectData).select();
 };
 
 /**
- *
- * @param file the user wants to add as a post
- * @returns file name
+ * Uploads a file to the storage bucket.
+ * @param {File | null} file - The file to be uploaded.
+ * @param {string} filePath - The path where the file will be uploaded.
+ * @returns {Promise<boolean>} A boolean indicating whether the upload was successful.
  */
 export const uploadFile = async (file: File | null, filePath: string) => {
 	if (file) {
@@ -104,9 +118,9 @@ export const uploadFile = async (file: File | null, filePath: string) => {
 };
 
 /**
- * Gets a single post from the database based on the post id
- * @param post_id
- * @returns
+ * Retrieves a single post from the database based on the post id.
+ * @param {string} post_id - The id of the post to retrieve.
+ * @returns {Promise<object>} The post object retrieved from the database.
  */
 export const fetchPost = async (post_id: string) => {
 	const { data } = await supabase.from("Posts").select().eq("id", post_id);
@@ -114,18 +128,18 @@ export const fetchPost = async (post_id: string) => {
 };
 
 /**
- * Increases the users upvotes
- * @param post_id
- * @param new_upvotes
+ * Increases the upvotes of a post in the database.
+ * @param {string} post_id - The id of the post to increase upvotes.
+ * @param {number} new_upvotes - The new number of upvotes.
  */
 export const increaseUpvotes = async (post_id: string, new_upvotes: number) => {
 	await supabase.from("Posts").update({ upvotes: new_upvotes }).eq("id", post_id);
 };
 
 /**
- * Delets a post from the database and removes the image as well
- * @param postId
- * @param filePath
+ * Deletes a post from the database and removes the associated image.
+ * @param {string} postId - The id of the post to delete.
+ * @param {string} filePath - The file path of the post image to remove.
  */
 export const deletePost = async (postId: string, filePath: string) => {
 	await supabase.from("Posts").delete().eq("id", postId);
@@ -133,16 +147,37 @@ export const deletePost = async (postId: string, filePath: string) => {
 	await supabase.from("Comments").delete().eq("post_id", postId);
 };
 
+/*****************************************************
+ *
+ *  Comment Related Methods
+ *
+ *****************************************************/
+
+/**
+ * Adds a comment to the database.
+ * @param {Object} obj - The comment object to add.
+ * @returns {Promise<{data: object, error: any}>} An object containing data and error properties.
+ */
 export const addComment = async (obj: Object) => {
 	const { data, error } = await supabase.from("Comments").insert(obj);
 	return { data, error };
 };
 
+/**
+ * Retrieves comments associated with a post from the database.
+ * @param {string} postId - The id of the post to retrieve comments for.
+ * @returns {Promise<{data: object, error: any}>} An object containing data and error properties.
+ */
 export const getComments = async (postId: string) => {
 	const { data, error } = await supabase.from("Comments").select().eq("post_id", postId);
 	return { data, error };
 };
 
+/**
+ * Deletes a comment from the database.
+ * @param {string} comment_id - The id of the comment to delete.
+ * @returns {Promise<{data: object, error: any}>} An object containing data and error properties.
+ */
 export const deleteCommentFromDatabase = async (comment_id: string) => {
 	const { data, error } = await supabase.from("Comments").delete().eq("id", comment_id);
 	return { data, error };
