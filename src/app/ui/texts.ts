1631cd90341bcb57c663c6a2819bfec2565aa06e
modusami <128193218+modusami@users.noreply.github.com>
2024-04-16 11:26:39 -0400

    Update texts.ts

--- src/app/ui/texts.ts
+++ src/app/ui/texts.ts
@@ -1,6 +1,7 @@
 export const defaultContentText = "...";
 
+// Post Previw Information
 export const postPreviewContentText =
-	"This is a sample description which servers as the content for your post.";
-export const postPreviewTitleText = "Sample Title Belongs Here...";
+	"Oh, let me tell you about the Dynamite Delight TNT Roll – it's absolutely mind-blowing! I just tried it for the first time, and oh boy, I'm hooked! Imagine sinking your teeth into this soft, golden pastry, and being hit with a burst of flavors that just explode in your mouth. It's like a party for your taste buds!";
+export const postPreviewTitleText = "TNT roll sushi 🍥";
 export const postPreviewImageFile = "/post_preview.jpg";
