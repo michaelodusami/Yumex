2ab906ea6b98947062a63aca2f97563488523458
modusami <128193218+modusami@users.noreply.github.com>
2024-04-16 11:00:51 -0400

    Update texts.ts

--- src/app/ui/texts.ts
+++ src/app/ui/texts.ts
@@ -3,4 +3,4 @@ export const defaultContentText = "...";
 export const postPreviewContentText =
 	"This is a sample description which servers as the content for your post.";
 export const postPreviewTitleText = "Sample Title Belongs Here...";
-export const postPreviewImageFile = "/exampletoast.jpg";
+export const postPreviewImageFile = "/post_preview.jpg";
