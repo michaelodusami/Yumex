5af8489a7eac6f421179964861b0b1cf4e20ae5a
modusami <128193218+modusami@users.noreply.github.com>
2024-04-16 11:00:56 -0400

    Update model.ts

--- src/app/lib/model.ts
+++ src/app/lib/model.ts
@@ -1,48 +1,7 @@
-// This file contains type definitions for your data.
-// It describes the shape of the data, and what data type each property should accept.
-/**
- * EXAMPLE:
- * export type User = {
-  id: string;
-  name: string;
-  email: string;
-  password: string;
-};
- */
+// model.ts
+// this file represents static data used throughout the application
 
-export type User = {
-	user_id: string;
-	username: string;
-	email: string;
-	password: string;
-};
-
-export type Post = {
-	id: string;
-	user_id: string;
-	title: string;
-	image: string;
-	content: string;
-	createdAt: Date;
-	upvotes: number;
-	category: string;
-	comments: Comment[];
-};
-
-export type Comment = {
-	user_id: string;
-	postId: string;
-	content: string;
-	createdAt: Date;
-};
-
-export type Upvote = {
-	userId: string;
-	postId: string;
-	createdAt: Date;
-};
-
-export const categories = {
+export const food_categories = {
 	asian: "asian",
 	mexican: "mexican",
 	italian: "italian",
