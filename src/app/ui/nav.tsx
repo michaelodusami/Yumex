2be3c4dd23bff6f616686d9e5c9e84fd107fb7c9
modusami <128193218+modusami@users.noreply.github.com>
2024-04-18 09:59:09 -0400

    Update Nav

--- src/app/ui/nav.tsx
+++ src/app/ui/nav.tsx
@@ -21,7 +21,7 @@ export default function Nav() {
 	return (
 		<nav
 			className={
-				"mx-auto md:flex md:items-center md:justify-between" +
+				"mx-auto md:flex md:items-center sm:border-b border-none md:justify-between" +
 				navMediumWidth +
 				navLargeWidth
 			}
