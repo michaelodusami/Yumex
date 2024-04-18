35bc4787639db2697dcc8929471331300e059f2a
modusami <128193218+modusami@users.noreply.github.com>
2024-04-17 21:48:07 -0400

    Update model.ts

--- src/app/lib/model.ts
+++ src/app/lib/model.ts
@@ -16,6 +16,17 @@ export const food_categories = {
 	pastry: "pastry",
 };
 
+export const food_category_images = {
+	asian: "/category_images/asian.png",
+	mexican: "/category_images/mexican.png",
+	italian: "/category_images/italian.png",
+	american: "/category_images/american.png",
+	african: "/category_images/african.png",
+	dessert: "/category_images/dessert.png",
+	healthy: "/category_images/healthy.png",
+	pastry: "/category_images/pastry.png",
+};
+
 /**
  * Object containing various routes used in the application.
  * Each route is represented by a key-value pair where the key is the route name and the value is the corresponding route path.
