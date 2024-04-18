8619d7f25578f5b297be89038655801b50d4181e
modusami <128193218+modusami@users.noreply.github.com>
2024-04-17 23:10:53 -0400

    Update CategorySelector

--- src/app/ui/components/CategorySelector.tsx
+++ src/app/ui/components/CategorySelector.tsx
@@ -16,36 +16,38 @@ const CategorySelector: React.FC<{ setSelectedCategory: any }> = ({ setSelectedC
 
 	return (
 		<>
-			<ul className="flex justify-between">
-				{categories.map((food_type, index) => {
-					const isActive = activeCategory === food_type;
-					return (
-						<>
-							<li
-								className={`flex flex-col items-center justify-center flex-1 transition-all ease-in-out duration-200 ${
-									isActive ? "font-bold scale-105" : ""
-								}`}
-							>
-								<button
-									onClick={handleSelectedCategory}
-									data-food-type={food_type}
-									className="flex items-center justify-center flex-col"
+			<div className="overflow-x-auto">
+				<ul className="flex justify-between whitespace-nowrap">
+					{categories.map((food_type, index) => {
+						const isActive = activeCategory === food_type;
+						return (
+							<>
+								<li
+									className={`flex flex-col items-center justify-center flex-1 transition-all ease-in-out duration-200 ${
+										isActive ? "font-bold scale-105" : ""
+									}`}
 								>
-									<Image
-										src={food_category_images[food_type]}
-										alt={food_type}
-										width={20}
-										height={20}
-									/>
-									<span className="capitalize text-[0.65rem] md:text-[1rem]">
-										{food_type}
-									</span>
-								</button>
-							</li>
-						</>
-					);
-				})}
-			</ul>
+									<button
+										onClick={handleSelectedCategory}
+										data-food-type={food_type}
+										className="flex items-center justify-center flex-col"
+									>
+										<Image
+											src={food_category_images[food_type]}
+											alt={food_type}
+											width={20}
+											height={20}
+										/>
+										<span className="capitalize text-[0.65rem] md:text-[1rem]">
+											{food_type}
+										</span>
+									</button>
+								</li>
+							</>
+						);
+					})}
+				</ul>
+			</div>
 		</>
 	);
 };
