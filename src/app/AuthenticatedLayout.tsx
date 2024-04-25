fdc9c93b0d6e627f47e70671c7470e3f665919c2
modusami <128193218+modusami@users.noreply.github.com>
2024-04-25 11:01:12 -0400

    Update AuthenticatedLayout.tsx

--- src/app/AuthenticatedLayout.tsx
+++ src/app/AuthenticatedLayout.tsx
@@ -6,6 +6,7 @@ import { supabase } from "./lib/server";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { useAuth } from "./ui/provider/AuthProvider";
 import { poppins } from "./ui/util/fonts";
+import "@/app/ui/styles/loginstyles.css";
 
 export default function AuthenticatedLayout({ children }: Readonly<{ children: React.ReactNode }>) {
 	const { session } = useAuth();
@@ -13,23 +14,25 @@ export default function AuthenticatedLayout({ children }: Readonly<{ children: R
 	if (!session) {
 		return (
 			<HomeContainer>
-				<div
-					className={
-						poppins.className +
-						" flex justify-center items-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-600"
-					}
-				>
-					<div className="w-full max-w-md p-8 bg-white rounded-lg shadow-lg m-3">
-						<div className="mb-8">
-							<h1 className="text-4xl font-bold text-center text-blue-600">Yumex</h1>
-						</div>
-						<div>
-							<h2 className="mb-4 text-2xl font-semibold">Sign In</h2>
-							<Auth
-								supabaseClient={supabase}
-								appearance={{ theme: ThemeSupa }}
-								providers={[]}
-							/>
+				<div className="loginbgimage">
+					<div
+						className={
+							poppins.className +
+							" flex justify-center items-center min-h-screen bg-black bg-opacity-50"
+						}
+					>
+						<div className="w-full max-w-md p-8 bg-white rounded-lg shadow-lg m-3">
+							<div className="mb-8">
+								<h1 className="text-4xl font-bold text-center text-black">Yumex</h1>
+							</div>
+							<div>
+								<h2 className="mb-4 text-2xl font-semibold">Sign In</h2>
+								<Auth
+									supabaseClient={supabase}
+									appearance={{ theme: ThemeSupa }}
+									providers={[]}
+								/>
+							</div>
 						</div>
 					</div>
 				</div>
