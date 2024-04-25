f51639e1d9e194ea3bb18572047e324eddbbd2e1
modusami <128193218+modusami@users.noreply.github.com>
2024-04-25 10:37:07 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
 import { inter } from "@/app/ui/util/fonts";
 import "./globals.css";
-import Nav from "@/app/ui/Nav";
+import Nav from "@/app/ui/nav";
+import { useAuth, AuthProvider } from "./ui/provider/AuthProvider";
+import { Auth } from "@supabase/auth-ui-react";
 import { SearchProvider } from "./ui/context/SearchContext";
+import { HomeContainer } from "./page";
+import { supabase } from "./lib/server";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 export const metadata: Metadata = {
 	title: "Yumex",
@@ -14,6 +19,29 @@ export default function RootLayout({
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	const { session } = useAuth();
+
+	if (!session) {
+		return (
+			<html lang="en">
+				<body className={inter.className + "antialiased"}>
+					<HomeContainer>
+						<div className="w-full lg:w-[50%] mt-5 mx-auto">
+							<div className="min-h-[80%] mx-auto border p-5 rounded-md">
+								<h1>Sign In</h1>
+								<Auth
+									supabaseClient={supabase}
+									appearance={{ theme: ThemeSupa }}
+									providers={[]}
+								/>
+							</div>
+						</div>
+					</HomeContainer>
+				</body>
+			</html>
+		);
+	}
+
 	return (
 		<html lang="en">
 			<body className={inter.className + "antialiased"}>
