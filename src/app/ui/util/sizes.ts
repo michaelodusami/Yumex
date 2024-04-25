30315b4175f14505ab059d3cb22d4fd4aa1d466b
modusami <128193218+modusami@users.noreply.github.com>
2024-04-25 16:38:46 -0400

    Update sizes.ts

--- src/app/ui/util/sizes.ts
+++ src/app/ui/util/sizes.ts
@@ -2,6 +2,6 @@ export const phoneIconWH = "w-[20px] h-[20px] ";
 export const phoneAvatarIconDropdownWH = "w-[35px] h-[35px] ";
 export const navMediumWidth = " md:w-[90%] ";
 export const navLargeWidth = " lg:w-[80%] ";
-export const singleColWidth = " md:w-[90%] lg:w-[60%] ";
+export const singleColWidth = " md:w-[70%] lg:w-[60%] ";
 export const POST_MIN_MAX_HEIGHT = " min-h-32 max-h-32 ";
 export const UPVOTE_SYMBOL_WH = " h-[30px] w-[30px] ";
