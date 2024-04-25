2d53776fa6bfb3961971ce219a24cec8bf9aaeac
modusami <128193218+modusami@users.noreply.github.com>
2024-04-25 10:31:44 -0400

    Update AuthProvider.tsx

--- src/app/ui/provider/AuthProvider.tsx
+++ src/app/ui/provider/AuthProvider.tsx
@@ -1,4 +1,33 @@
 "use client";
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
 import { supabase } from "@/app/lib/server";
+
 const AuthContext = createContext<any | null>(null);
+
+const AuthProvider: React.FC<{ children: any }> = ({ children }) => {
+	const [session, setSession] = useState<any>(null);
+
+	useEffect(() => {
+		supabase.auth.getSession().then(({ data: { session } }) => {
+			setSession(session);
+		});
+
+		const {
+			data: { subscription },
+		} = supabase.auth.onAuthStateChange((_event, session) => {
+			setSession(session);
+		});
+
+		return () => subscription.unsubscribe();
+	}, []);
+
+	return <AuthContext.Provider value={{ session }}>{children}</AuthContext.Provider>;
+};
+
+const useAuth = () => {
+	const authContext = useContext(AuthContext);
+	if (!authContext) {
+		throw new Error("useAuth must be used within an AuthProvider");
+	}
+	return authContext;
+};
