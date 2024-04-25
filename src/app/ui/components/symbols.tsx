f0b8c933cc45f196af6d8a4de0d292e99db11560
modusami <128193218+modusami@users.noreply.github.com>
2024-04-25 13:18:34 -0400

    Update symbols.tsx
    Create Text Bubble Symbol

--- src/app/ui/components/symbols.tsx
+++ src/app/ui/components/symbols.tsx
@@ -1,5 +1,9 @@
-import { HandThumbUpIcon } from "@heroicons/react/24/outline";
+import { HandThumbUpIcon, ChatBubbleBottomCenterTextIcon } from "@heroicons/react/24/outline";
 
 export const UpvoteSymbol: React.FC<{ styles: string }> = ({ styles = "" }) => {
 	return <HandThumbUpIcon className={styles} />;
+};
+
+export const ChatBubbleSymbol: React.FC<{ styles: string }> = ({ styles = "" }) => {
+	return <ChatBubbleBottomCenterTextIcon className={styles} />;
 };
