2bf51858ffed933f174c1a86cbaf1354b481ef3b
modusami <128193218+modusami@users.noreply.github.com>
2024-04-26 22:49:32 -0400

    Update AvatarLogo.tsx

--- src/app/ui/components/AvatarLogo.tsx
+++ src/app/ui/components/AvatarLogo.tsx
@@ -1,14 +1,14 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const AvatarLogo: React.FC<{ src: string; fallback: string; styles: string }> = ({
-	src = "https://github.com/shadcn.png",
-	fallback = "CN",
+	src = "https://github.com/shadcn.pngj",
+	fallback = "YM",
 	styles = "",
 }) => {
 	return (
 		<Avatar className={styles}>
 			<AvatarImage src={src} />
-			<AvatarFallback>YM</AvatarFallback>
+			<AvatarFallback>{fallback}</AvatarFallback>
 		</Avatar>
 	);
 };
