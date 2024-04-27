3b65b4af1602d1709d826996fb63197fe659c896
modusami <128193218+modusami@users.noreply.github.com>
2024-04-26 23:37:00 -0400

    Update Category.tsx

--- src/app/ui/components/Category.tsx
+++ src/app/ui/components/Category.tsx
@@ -3,12 +3,10 @@ import { categoryColors } from "../util/colors";
 const Category: React.FC<{ category: string }> = ({ category = "NONE" }) => {
 	return (
 		<span
-			style={{
-				backgroundColor: categoryColors[category !== "" ? category : "asian"],
-			}}
-			className="uppercase border rounded-lg p-2 text-black"
+			style={{ backgroundColor: categoryColors[category] }}
+			className="inline-block px-3 py-1 rounded-full text-sm font-semibold uppercase"
 		>
-			{category !== "" ? category : "asian"}
+			{category}
 		</span>
 	);
 };
