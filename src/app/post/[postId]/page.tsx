11cec464b833859cedd78e22fd660644c9ad86ff
modusami <128193218+modusami@users.noreply.github.com>
2024-04-28 01:18:01 -0400

    Update page.tsx | Post Page
    Make comments respond well to session

--- src/app/post/[postId]/page.tsx
+++ src/app/post/[postId]/page.tsx
@@ -11,6 +11,8 @@ import { increaseUpvotes, addComment, getComments } from "@/app/lib/data";
 import { getIdFromUser, deletePost } from "@/app/lib/data";
 import Comment from "@/app/ui/components/Comment";
 import AuthenticatedLayout from "@/app/AuthenticatedLayout";
+import { useAuth } from "@/app/ui/provider/AuthProvider";
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
 import {
 	EllipsisHorizontalCircleIcon,
 	ChatBubbleBottomCenterTextIcon,
@@ -28,6 +30,8 @@ import { all_routes } from "@/app/lib/model";
 import { navLargeWidth, navMediumWidth, singleColWidth } from "@/app/ui/util/sizes";
 
 const Page: React.FC<{ params: any }> = ({ params }) => {
+	const { session } = useAuth();
+	const fakeSession = false;
 	const postId = params.postId;
 	const [post, setPost] = useState<any>(null);
 	const [postUpvotes, setPostUpvotes] = useState<any>(null);
@@ -119,103 +123,121 @@ const Page: React.FC<{ params: any }> = ({ params }) => {
 	}
 
 	return (
-		<AuthenticatedLayout>
-			<HomeContainer>
-				<div className="max-w-3xl mx-auto p-2 md:p-8">
-					<div className=" shadow-md rounded-lg p-8">
-						<div className="flex justify-between">
-							<div className="flex items-center mb-8">
-								<div className="mr-6">
-									<AvatarLogo src={""} fallback={""} styles={""} />
+		<HomeContainer>
+			<div className="max-w-3xl mx-auto p-2 md:p-8">
+				<div className=" shadow-md rounded-lg p-8">
+					<div className="flex justify-between">
+						<div className="flex items-center mb-8">
+							<div className="mr-6">
+								<AvatarLogo src={""} fallback={""} styles={""} />
+							</div>
+							<div>
+								<div className="text-xl font-semibold">
+									<AsyncUserEmail user_id={post.user_id} />
 								</div>
-								<div>
-									<div className="text-xl font-semibold">
-										<AsyncUserEmail user_id={post.user_id} />
-									</div>
-									<div className="text-gray-500">
-										Posted At: {getFormattedDate(post.created_at)}
-									</div>
+								<div className="text-gray-500">
+									Posted At: {getFormattedDate(post.created_at)}
 								</div>
 							</div>
-
-							{showEditDeleteButton && (
-								<DropdownMenu>
-									<DropdownMenuTrigger>
-										<EllipsisHorizontalCircleIcon className="w-6 h-6 text-gray-500 hover:text-gray-700 cursor-pointer" />
-									</DropdownMenuTrigger>
-									<DropdownMenuContent>
-										<DropdownMenuLabel>Post Options</DropdownMenuLabel>
-										<DropdownMenuSeparator />
-										<DropdownMenuItem onClick={handleDeletePost}>
-											Delete Post
-										</DropdownMenuItem>
-									</DropdownMenuContent>
-								</DropdownMenu>
-							)}
 						</div>
 
-						<div className="text-3xl font-bold mb-4 flex justify-between items-center">
-							<h1>{post.title}</h1>
-							<Category category={post.category} />
-						</div>
-						<div className="mb-8">
-							<AsyncImage
-								filepath={post.post_image_filepath}
-								title={post.title}
-								styles={""}
-							/>
-						</div>
-						<div className="text-lg mb-8">
-							<p>{post.content}</p>
-						</div>
-						<div className="flex items-center justify-between">
-							<div className="flex items-center">
-								<button
-									className="flex items-center text-gray-500 hover:text-blue-500 transition duration-300"
-									onClick={handleUpvotes}
-								>
-									<UpvoteSymbol styles="w-6 h-6 mr-1" />
-									<span>{postUpvotes}</span>
-								</button>
-							</div>
-						</div>
+						{showEditDeleteButton && (
+							<DropdownMenu>
+								<DropdownMenuTrigger>
+									<EllipsisHorizontalCircleIcon className="w-6 h-6 text-gray-500 hover:text-gray-700 cursor-pointer" />
+								</DropdownMenuTrigger>
+								<DropdownMenuContent>
+									<DropdownMenuLabel>Post Options</DropdownMenuLabel>
+									<DropdownMenuSeparator />
+									<DropdownMenuItem onClick={handleDeletePost}>
+										Delete Post
+									</DropdownMenuItem>
+								</DropdownMenuContent>
+							</DropdownMenu>
+						)}
 					</div>
-					<div className="mt-12">
-						<h3 className="text-xl font-semibold mb-4">Comments</h3>
-						<form className="mb-8 flex gap-4" onSubmit={onSubmit}>
-							<input
-								type="text"
-								placeholder="Add a comment"
-								onChange={onChange}
-								value={comment}
-								className="flex-1 px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-							/>
+
+					<div className="text-3xl font-bold mb-4 flex justify-between items-center">
+						<h1>{post.title}</h1>
+						<Category category={post.category} />
+					</div>
+					<div className="mb-8">
+						<AsyncImage
+							filepath={post.post_image_filepath}
+							title={post.title}
+							styles={""}
+						/>
+					</div>
+					<div className="text-lg mb-8">
+						<p>{post.content}</p>
+					</div>
+					<div className="flex items-center justify-between">
+						<div className="flex items-center">
+							{/* handle session */}
 							<button
-								type="submit"
-								className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition duration-300"
+								className="flex items-center text-gray-500 hover:text-blue-500 transition duration-300"
+								onClick={handleUpvotes}
 							>
-								Submit
+								<UpvoteSymbol styles="w-6 h-6 mr-1" />
+								<span>{postUpvotes}</span>
 							</button>
-						</form>
-						<div className="space-y-6">
-							{commentList
-								.sort((a: any, b: any) => {
-									const aDate = new Date(a.created_at);
-									const bDate = new Date(b.created_at);
-									return +bDate - +aDate;
-								})
-								.map((comment: any) => (
-									<Comment
-										key={comment.id}
-										comment={comment}
-										setCommentList={setCommentList}
-									/>
-								))}
 						</div>
 					</div>
 				</div>
-			</HomeContainer>
-		</AuthenticatedLayout>
+				<div className="mt-12">
+					<h3 className="text-xl font-semibold mb-4">Comments:</h3>
+					{/* handle session */}
+					{fakeSession ? (
+						<>
+							<form className="relative mb-8" onSubmit={onSubmit}>
+								<textarea
+									placeholder="Start typing here to add a comment!"
+									name="comment"
+									id="comment"
+									value={comment}
+									onChange={onChange}
+									className="w-full p-3 resize-none rounded-md border"
+									rows={3}
+								></textarea>
+								{comment != "" && (
+									<button
+										type="submit"
+										className="p-2 text-sm border rounded-md hover:bg-blue-600 transition duration-300 absolute bottom-3 right-3"
+									>
+										Submit
+									</button>
+								)}
+							</form>
+						</>
+					) : (
+						<div>
+							<button className="transition-all ease-in-out duration-500 flex gap-2 items-center justify-center p-3 border rounded-2xl w-full hover:font-bold ">
+								<span>Click to add a button</span>
+								<span>
+									<PlusCircleIcon className="w-[20px] font-bold" />
+								</span>
+							</button>
+						</div>
+					)}
+
+					<div className="space-y-6">
+						{commentList
+							.sort((a: any, b: any) => {
+								const aDate = new Date(a.created_at);
+								const bDate = new Date(b.created_at);
+								return +bDate - +aDate;
+							})
+							.map((comment: any) => (
+								<Comment
+									key={comment.id}
+									comment={comment}
+									setCommentList={setCommentList}
+								/>
+							))}
+					</div>
+				</div>
+			</div>
+		</HomeContainer>
 	);
 };
 
