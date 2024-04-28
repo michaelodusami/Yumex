2155d5e16d9827230dc0a7cdbe18302212a378fd
modusami <128193218+modusami@users.noreply.github.com>
2024-04-28 01:34:22 -0400

    Update LoginModal.tsx

--- src/app/ui/components/LoginModal.tsx
+++ src/app/ui/components/LoginModal.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { supabase } from "@/app/lib/server";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { Auth } from "@supabase/auth-ui-react";
+import { XMarkIcon } from "@heroicons/react/24/solid";
 
 const LoginModal: React.FC<{ setShowModal: any }> = ({ setShowModal }) => {
 	const closeModal = () => {
@@ -14,29 +15,25 @@ const LoginModal: React.FC<{ setShowModal: any }> = ({ setShowModal }) => {
 				<div className="absolute inset-0 bg-gray-900 opacity-50" onClick={closeModal}></div>
 				<div className="relative bg-white p-8 rounded shadow-lg max-w-md w-full">
 					<button
-						className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+						className="absolute top-2 right-2 hover:text-red-500 hover:font-bold flex justify-end  "
 						onClick={closeModal}
 					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							className="h-6 w-6"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth={2}
-								d="M6 18L18 6M6 6l12 12"
-							/>
-						</svg>
+						<span className="h-5 w-5">
+							<XMarkIcon className="" />
+						</span>
 					</button>
-					<Auth
-						supabaseClient={supabase}
-						appearance={{ theme: ThemeSupa }}
-						providers={[]}
-					/>
+
+					<div className="mb-8">
+						<h1 className="text-4xl font-bold text-center text-black">Yumex</h1>
+					</div>
+					<div>
+						{/* <h2 className="mb-4 text-2xl font-semibold">Welcome</h2> */}
+						<Auth
+							supabaseClient={supabase}
+							appearance={{ theme: ThemeSupa }}
+							providers={[]}
+						/>
+					</div>
 				</div>
 			</div>
 		</>
