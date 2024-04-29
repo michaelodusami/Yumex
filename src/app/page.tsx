1ac4829399731771cab8da262c201b545487cba2
modusami <128193218+modusami@users.noreply.github.com>
2024-04-28 22:36:10 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-"use client";
-
-import Forum from "../components/ui/Forum";
-export default function Home() {
+import Forum from "@/components/ui/Forum";
+export default function Page() {
 	return (
 		<main className="min-h-screen">
 			<div className="w-full md:w-[100%] lg:w-[100%] min-h-screen mx-auto flex-col flex">
